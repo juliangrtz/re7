56a6260f9684c5850fbefb4335d3be1aa6ccfa3d
juliangrtz <julian.goeritz@protonmail.com>
2026-02-28 23:46:51 +0100

    Add REFramework.NET project

---
+++ src/Biohazard.BioRand.RE7.REFrameworkPlugins/TestPlugin.cs
@@ -0,0 +1,16 @@
+﻿namespace Biohazard.BioRand.RE7.REFrameworkPlugins;
+
+using REFrameworkNET;
+using REFrameworkNET.Attributes;
+
+public class REFrameworkPlugin
+{
+    [PluginEntryPoint]
+    public static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            API.LogInfo("Hello from BioRand 7!");
+        }
+    }
+}
