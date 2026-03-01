ff67a2807f22d016472775939eff6457eb5439eb
juliangrtz <julian.goeritz@protonmail.com>
2026-03-01 23:27:33 +0100

    Port REFramework Lua mods to C#

---
+++ src/Biohazard.BioRand.RE7.REFrameworkPlugins/REFPlugin.cs
@@ -0,0 +1,140 @@
+﻿namespace Biohazard.BioRand.RE7.REFrameworkPlugins;
+
+using app;
+using Hexa.NET.ImGui;
+using REFrameworkNET;
+using REFrameworkNET.Attributes;
+using REFrameworkNET.Callbacks;
+using static app.InventoryMenu;
+using static Logger;
+
+public class REFPlugin
+{
+    private static bool IsInitialized = false;
+
+    [PluginEntryPoint]
+    public static void Main()
+        => Initialize();
+
+    private static void Initialize()
+    {
+        ImGuiDrawUI.Post += OnImGuiDrawUi;
+        IsInitialized = true;
+    }
+
+    #region Inventory
+
+    public static int DesiredEthanExtendLv { get; set; }
+    public static int DesiredMiaExtendLv { get; set; }
+
+    private static string? GetPlayerName()
+    {
+        var objectManager = API.GetManagedSingleton("app.ObjectManager");
+        if (objectManager == null)
+            return null;
+
+        var playerObj = objectManager.GetField("PlayerObj") as ManagedObject;
+        if (playerObj == null)
+            return null;
+
+        return playerObj.Call("get_Name") as string;
+    }
+
+    private static int? GetDesiredInventorySize()
+    {
+        var playerName = GetPlayerName();
+        if (playerName == null) return null;
+
+        if (playerName.StartsWith("Pl00", StringComparison.Ordinal))
+        {
+            return DesiredEthanExtendLv;
+        }
+        else if (playerName.StartsWith("Pl2", StringComparison.Ordinal))
+        {
+            return DesiredMiaExtendLv;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    [MethodHook(typeof(Inventory), nameof(Inventory.setupItemSlotManager), MethodHookType.Pre)]
+    private static PreHookResult Inventory_ctor_Pre(Span<ulong> args)
+    {
+        var inventory = ManagedObject.ToManagedObject(args[1]).As<Inventory>()!;
+
+        var desiredLevel = GetDesiredInventorySize();
+        if (desiredLevel == null)
+            return PreHookResult.Continue;
+
+        if ((int)inventory.ExtendLv < desiredLevel)
+        {
+            Log("Increase Inventory._ExtendLv");
+            inventory._ExtendLv = (Inventory.ExtendLvDef)desiredLevel.Value;
+        }
+        return PreHookResult.Continue;
+    }
+
+    [MethodHook(typeof(Inventory), nameof(Inventory.setExtendLv), MethodHookType.Pre)]
+    private static PreHookResult Inventory_setExtendLv_Pre(Span<ulong> args)
+    {
+        var newLevel = (int)args[2];
+        var desiredLevel = GetDesiredInventorySize();
+        if (desiredLevel == null)
+            return PreHookResult.Continue;
+
+        if (newLevel < (int)desiredLevel)
+        {
+            Log("Prevent Inventory._ExtendLv shrinking");
+            args[2] = (ulong)desiredLevel;
+        }
+
+        return PreHookResult.Continue;
+    }
+
+    private const int MaxCombineUIRowNum = 5;
+
+    [MethodHook(typeof(DictionaryCombineUIController), nameof(DictionaryCombineUIController.deactivate), MethodHookType.Pre)]
+    private static PreHookResult DictionaryCombineUIController_deactivate_Pre(Span<ulong> args)
+    {
+        var controller = ManagedObject.ToManagedObject(args[1]).As<DictionaryCombineUIController>();
+        var type = TDB.Get().FindType("app.InventoryMenu.DictionaryCombineUIController");
+        var field = type.GetField("RowNum");
+        var current = (int)field.GetDataBoxed(controller.Address(), false);
+
+        if (current != MaxCombineUIRowNum)
+        {
+            Log($"Patch DictionaryCombineUIController.RowNum from {current} to {MaxCombineUIRowNum}");
+            field.SetDataBoxed(controller.Address(), MaxCombineUIRowNum, false);
+        }
+
+        return PreHookResult.Continue;
+    }
+
+    [MethodHook(typeof(InventoryMenu), nameof(InventoryMenu.DictionaryCombine_UnlockedCombine), MethodHookType.Post)]
+    private static void InventoryMenu_DictionaryCombine_UnlockedCombine_Post(ref ulong retval)
+    {
+        if (retval != 1)
+        {
+            Log($"[PATCH] InventoryMenu.DictionaryCombine_UnlockedCombine from false to true");
+            retval = 1;
+        }
+    }
+
+    #endregion Inventory
+
+    #region UI
+
+    private static void OnImGuiDrawUi()
+    {
+        if (!IsInitialized) return;
+
+        if (ImGui.TreeNode("BioRand 7"))
+        {
+            ImGui.TreePop();
+        }
+    }
+
+    #endregion UI
+}
