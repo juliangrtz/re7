d601d92c9b5919cb1e3a578aeda09c3d1572762e
juliangrtz <julian.goeritz@protonmail.com>
2026-03-15 22:48:08 +0100

    Implement generator for via.* types in scene files

---
+++ src/Biohazard.BioRand.RE7.DataGen/Commands/FixRszFileCommand.cs
@@ -0,0 +1,36 @@
+﻿using Biohazard.BioRand.RE7.DataGen.CodeGen;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using static Biohazard.BioRand.RE7.DataGen.Commands.FixRszFileCommand;
+
+namespace Biohazard.BioRand.RE7.DataGen.Commands;
+
+internal sealed class FixRszFileCommand : Command<Settings>
+{
+    internal sealed class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<rszInputFile>")]
+        public string RszInputFile { get; set; } = "";
+
+        [CommandArgument(1, "<il2cppDumpFile>")]
+        public string Il2CppJsonDumpFile { get; set; } = "";
+
+        [CommandArgument(2, "<typeName>")]
+        public string OutputFileName { get; set; } = "";
+    }
+
+    public override int Execute(CommandContext context, Settings settings, CancellationToken token)
+    {
+        try
+        { 
+            // TODO
+            //AnsiConsole.MarkupLine($"[green]Generated[/] {settings.OutputFileName}");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.MarkupLine($"[red]Error:[/] {ex.Message}");
+            return -1;
+        }
+    }
+}
