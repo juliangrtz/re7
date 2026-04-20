b941ccc6dbd76c1eb4f6aa31d552a18edea5d5db
juliangrtz <julian.goeritz@protonmail.com>
2026-04-20 17:40:17 +0200

    Add BioRand touch to Jack's attic punch
    Welcome to BioRand 7, son!

---
+++ src/Biohazard.BioRand.RE7/Patches/BiorandJackPunchPatch.cs
@@ -0,0 +1,7 @@
+﻿namespace Biohazard.BioRand.RE7.Patches;
+
+internal class BiorandJackPunchPatch(IPatchContext context) : IPatch
+{
+    public void Apply() 
+        => context.ApplyOverlay(context.GetSupplementFile("biorand_jackpunch.zip")!);
+}
