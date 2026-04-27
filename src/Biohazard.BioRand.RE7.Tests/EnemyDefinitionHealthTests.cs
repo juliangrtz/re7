576d99094bfbeec0401416e2857db48753487b8e
juliangrtz <julian.goeritz@protonmail.com>
2026-04-27 21:13:35 +0200

    Simplify extra enemy logic (for now)

--- src/Biohazard.BioRand.RE7.Tests/EnemyDefinitionHealthTests.cs
+++ src/Biohazard.BioRand.RE7.Tests/EnemyDefinitionHealthTests.cs
@@ -2,9 +2,7 @@ using Biohazard.BioRand.RE7.Enemies;
 using Biohazard.BioRand.RE7.Enemies.Impl;
 using Biohazard.BioRand.RE7.Modifiers;
 using IntelOrca.Biohazard.BioRand;
-using IntelOrca.Biohazard.REE.Rsz;
-using System.Numerics;
-using System.Reflection;
+
 namespace Biohazard.BioRand.RE7.Tests;
 
 public class EnemyDefinitionHealthTests
@@ -76,67 +74,6 @@ public class EnemyDefinitionHealthTests
         Assert.Equal(enemy.BaseHealth, firstHealth);
         Assert.NotEqual(firstHealth, secondHealth);
         Assert.True(secondHealth > firstHealth);
-    }
-
-    [Fact]
-    public void AddEnemyToGenerator_UsesRandomizedHealthForNewSpawnInfos()
-    {
-        using var result = RandomizerTest.RunState(config =>
-        {
-            config["enemy-random-health"] = true;
-            config["enemy-health-min-molded"] = 2.0;
-            config["enemy-health-max-molded"] = 2.0;
-        });
-
-        var modifier = new EnemyModifier();
-        var addEnemyToGenerator = typeof(EnemyModifier).GetMethod("AddEnemyToGenerator", BindingFlags.Instance | BindingFlags.NonPublic);
-        Assert.NotNull(addEnemyToGenerator);
-
-        IEnemyDefinition definition = EnemyDefinitions.Instance.All.OfType<Molded>().Single();
-        var placement = new EnemyModifier.ExtraEnemyPlacement
-        {
-            Id = definition.EnemyId.ToString()
-        };
-        var options = new EnemyModifier.EnemyRandomizerOptions(
-            EnemyVariety: 1,
-            MaxPackSize: 1,
-            DebugUniqueHp: false,
-            IsBalanced: false,
-            ProgressiveDifficulty: false,
-            ScaleOptions: new EnemyModifier.ScaleOptions(0.0, 1.0f, 1.0f)
-        );
-        var transform = new via.Transform
-        {
-            Position = Vector3.Zero,
-            Rotation = Quaternion.Identity,
-            Scale = Vector3.One
-        };
-        var healthResolver = new EnemyModifier.EnemyHealthResolver(result.Randomizer, options, result.Randomizer.GetRng("modifier/enemy-health"));
-        var logger = new RandomizerLogger();
-
-        var generator = result.Randomizer.TemplateService.GetObject("EnemyGenerator").Clone();
-        var invocationResult = addEnemyToGenerator!.Invoke(
-            modifier,
-            [
-                result.Randomizer,
-                logger,
-                generator,
-                placement,
-                definition,
-                transform,
-                options,
-                result.Randomizer.GetRng("modifier/enemy-scale"),
-                healthResolver
-            ]);
-
-        Assert.NotNull(invocationResult);
-        var (updatedGenerator, spawnInfoGuid) = ((RszGameObject, Guid))invocationResult!;
-        var spawnInfoGameObject = updatedGenerator.FindGameObject(spawnInfoGuid);
-        Assert.NotNull(spawnInfoGameObject);
-
-        var spawnInfo = spawnInfoGameObject!.FindComponent<app.EnemySpawnInfo>();
-        Assert.NotNull(spawnInfo);
-        Assert.Equal(definition.BaseHealth * 2.0f, spawnInfo!.HealthParameter.Health);
     }
 
     private static Randomizer CreateRandomizer(Action<RandomizerConfiguration> configure)
