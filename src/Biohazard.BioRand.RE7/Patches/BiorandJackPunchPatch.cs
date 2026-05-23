c2a488d6a4a61a3cb5649533559318b6e1f06f76
juliangrtz <julian.goeritz@protonmail.com>
2026-05-23 22:37:25 +0200

    Refactor overlay patches

--- src/Biohazard.BioRand.RE7/Patches/BiorandJackPunchPatch.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Biohazard.BioRand.RE7.Patches;
-
-internal class BiorandJackPunchPatch(IPatchContext context) : IPatch {
-    public void Apply()
-        => context.ApplyOverlay(context.GetSupplementFile("biorand_jackpunch.zip")!);
-}
